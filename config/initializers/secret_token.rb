ddda17b3113107a90a35e27265e5aa25bc7a2252
Jason Bellows <jason@targetedlearning.com>
2010-11-19 01:21:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LambertApp::Application.config.secret_token = '183dee9ecc3728bb490b757cdebf0f3b243aed424bd0b2d1cc7dd642c95fd547defb60921bcf7a6d0f6baabb47d01466fd3ef0932fea781091995d7cd05fb31f'
